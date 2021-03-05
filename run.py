49055a6c2646bb54c8d03816bbed9c2474a08a25
wyx <Xuan594@126.com>
2021-03-05 17:16:33 +0800

    fi

--- run.py
+++ run.py
@@ -1,80 +1,55 @@
+import re
+import time
+
 import requests
 from lxml import etree
-
+from .setting import *
 from pymongo import MongoClient
 
 client = MongoClient('mongodb://root:123456@172.16.10.136:27017/')
 db = client['spider']
 collection = db['result']
+params = {}
+for i in collection.find({'is_crawl': {'$exists': False}}):
+    try:
 
+        url = 'https://www.315jiage.cn/n{}.aspx'.format(i['id'])
+        # time.sleep(1)
 
-cookies = {
-    'HMACCOUNT_BFESS': '98606595895E1C2C',
-    'BAIDUID_BFESS': '32E8EFF771C1D3601D855D6311D9C98F:FG=1',
-}
+        response = requests.post(url, headers=headers, params=params, cookies=cookies, data=data, proxies=proxies)
+        print(response.status_code)
+        if response.status_code == 200:
+            response.encoding = 'utf8'
+            root = etree.HTML(response.content)
 
-headers = {
-    'authority': 'www.315jiage.cn',
-    'cache-control': 'no-cache',
-    'sec-ch-ua': '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
-    'sec-ch-ua-mobile': '?0',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36',
-    'accept': '*/*',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-user': '?1',
-    'sec-fetch-dest': 'empty',
-    'referer': 'https://www.315jiage.cn/n171487.aspx',
-    'accept-language': 'zh-CN,zh;q=0.9',
-    'cookie': '__cfduid=d28e514d3b45380fb95958c27498274c11613642406; iwmsGid=0FVZL399S8DAZVXFHT4S; Hm_lvt_4cce664ec5d8326cc457ab09053c15b2=1613642407,1613642563,1613973475; cf_clearance=f22347c6964411f83ead2b3f1ec6144a4d2dc7a8-1614320772-0-250; rtv=52A57B,8207652; Hm_lpvt_4cce664ec5d8326cc457ab09053c15b2=1614332577',
-    'Referer': 'https://www.315jiage.cn/n171487.aspx',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36',
-    'Connection': 'keep-alive',
-    'Accept': 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
-    'Sec-Fetch-Site': 'cross-site',
-    'Sec-Fetch-Mode': 'no-cors',
-    'Sec-Fetch-Dest': 'image',
-    'Accept-Language': 'zh-CN,zh;q=0.9',
-    'x-requested-with': 'XMLHttpRequest',
-    'pragma': 'no-cache',
-    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'origin': 'https://www.315jiage.cn',
-}
+            commodity_name_list = root.xpath('//*[@class="block-info-prop text-oneline"]/*[@itemprop="name"]/text()')
+            commodity_name = ''.join(commodity_name_list[0].split())
+            commodity_name_new = commodity_name.strip()
 
-data = {
-  'cmd': 'logVisit',
-  'id': '171487'
-}
+            commodity_code_list = root.xpath('//div/span[contains(text(), "条形码")]/following::text()')
+            commodity_code = ''.join(commodity_code_list[0].split())
 
-params ={}
+            approval_no_list = root.xpath('//div[@class="block-info-prop text-oneline"]/u/a/text()')
+            approval_no = ''.join(approval_no_list[0].split())
 
-#NB. Original query string below. It seems impossible to parse and
-#reproduce query strings 100% accurately so the one below is given
-#in case the reproduced version is not "correct".
-# response = requests.post('https://www.315jiage.cn/n364233.aspx?__cf_chl_captcha_tk__=0abb4a9aea84c6ee33363acc04397fffefcadff0-1614130863-0-Aeq-nGjXogvMxWsQtCOzKevQ7Yt8QbjYh-RnoR9OYzkKP4y3Mbnb9OZHEoFaOd6_nQDuDCDpfkzj1c2n4OI3VXHrxzWA14ftOOHSg6ecY-aIRmZVUPMKUch5yw90wclpJcmDYpn-NpxWV-gEeJj5zBpORDkKiirrjOf778ubq4StdEnJzVsxfchDbWJmNssoDAhIReRZ0cnrJhnl-RwMJ-k4LRH7vPR80DqOXF6IbzCP4sUyn0X13oilM_zf4N3ytUYkVLO9k6uFOSWFRqS1WDz7w4KWo7X2_-KrzQl3Xaa_Hjf3NmsrR6N5Iau2yi6PpS_ueoT5Ypne6wYsBc0fniqCuHTxa9OVcMsRiI_WyPUs', headers=headers, cookies=cookies, data=data)
+            guige_list = root.xpath('//div/span[contains(text(), "规格：")]/following::text()')
+            guige = ''.join(guige_list[0].split())
 
+            baozhuangdanwei_list = root.xpath('//div/span[contains(text(), "包装单位：")]/following::text()')
+            baozhuangdanwei = ''.join(baozhuangdanwei_list[0].split())
 
-# url = 'https://www.315jiage.cn/n325021.aspx'
-# url = 'https://www.315jiage.cn/n327184.aspx'
-# url = 'https://www.315jiage.cn/n134488.aspx'
-# for i in range(2000, 400000):
-for i in range(364647, 400000):
-    url = 'https://www.315jiage.cn/n{}.aspx'.format(i)
-    response = requests.post(url, headers=headers, params=params, cookies=cookies, data=data)
-    print(response.status_code)
-    if response.status_code == 200:
-        print(url)
-        response.encoding = 'utf8'
-        root = etree.HTML(response.content)
-        commodity_name_list = root.xpath('//*[@class="block-info-prop text-oneline"]/*[@itemprop="name"]/text()')
-        commodity_code_list = root.xpath('//div/span[contains(text(), "条形码")]/following::text()')
-        approval_no_list = root.xpath('//div[@class="block-info-prop text-oneline"]/u/a/text()')
-        if commodity_name_list:
-            try:
-                result = {'id': i,'commodity_name': commodity_name_list[0], 'commodity_code': commodity_code_list[0], 'approval_no': approval_no_list[0]}
-                collection.update({'id': i}, result, upsert=True)
-                print(result)
-            except:
-                pass
+            shengchanchangjia_list = root.xpath('//div/span[contains(text(), "生产厂家：")]/following::text()')
+            shengchanchangjia = ''.join(shengchanchangjia_list[0].split())
 
+            if commodity_name_list:
+                try:
+                    result = {'id': i['id'], 'commodity_name': commodity_name, 'commodity_code': commodity_code,
+                              'approval_no': approval_no, 'guige': guige, 'danwei': baozhuangdanwei,
+                              'changjia': shengchanchangjia, 'is_crawl': 1}
+                    collection.update({'id': i['id']}, result, upsert=True)
+                    print(result)
+                except Exception as ex:
+                    print(ex)
+    except Exception as ex:
+        print(ex)
+        pass
---
+++ setting.py
@@ -0,0 +1,60 @@
+
+cookies = {
+    'HMACCOUNT_BFESS': '98606595895E1C2C',
+    'BAIDUID_BFESS': '32E8EFF771C1D3601D855D6311D9C98F:FG=1',
+}
+
+headers = {
+    'authority': 'www.315jiage.cn',
+    'cache-control': 'no-cache',
+    'sec-ch-ua': '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36',
+    'accept': '*/*',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-user': '?1',
+    'sec-fetch-dest': 'empty',
+    'referer': 'https://www.315jiage.cn/n171487.aspx',
+    'accept-language': 'zh-CN,zh;q=0.9',
+    'cookie': '__cfduid=d28e514d3b45380fb95958c27498274c11613642406; iwmsGid=0FVZL399S8DAZVXFHT4S; Hm_lvt_4cce664ec5d8326cc457ab09053c15b2=1613642407,1613642563,1613973475; cf_clearance=f22347c6964411f83ead2b3f1ec6144a4d2dc7a8-1614320772-0-250; rtv=52A57B,8207652; Hm_lpvt_4cce664ec5d8326cc457ab09053c15b2=1614332577',
+    'Referer': 'https://www.315jiage.cn/n171487.aspx',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36',
+    'Connection': 'keep-alive',
+    'Accept': 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+    'Sec-Fetch-Site': 'cross-site',
+    'Sec-Fetch-Mode': 'no-cors',
+    'Sec-Fetch-Dest': 'image',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'x-requested-with': 'XMLHttpRequest',
+    'pragma': 'no-cache',
+    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'origin': 'https://www.315jiage.cn',
+}
+
+data = {
+  'cmd': 'logVisit',
+  'id': '171487'
+}
+
+
+
+proxyHost = "http-dyn.abuyun.com"
+proxyPort = "9020"
+
+# 代理隧道验证信息
+proxyUser = "H5UWW33P9X0EWXWD"
+proxyPass = "8F486DBEB580CBCB"
+
+proxyMeta = "http://%(user)s:%(pass)s@%(host)s:%(port)s" % {
+    "host": proxyHost,
+    "port": proxyPort,
+    "user": proxyUser,
+    "pass": proxyPass,
+}
+
+proxies = {
+    "http": proxyMeta,
+    "https": proxyMeta,
+}
